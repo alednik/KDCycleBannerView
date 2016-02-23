d617d7293b5ccffc729a062729dd03b3949b3117
Alex Lednik <alexlednik@Alexs-MacBook-Pro.local>
2016-02-23 12:48:08 +0300

    pageControl property added

--- KDCycleBannerView/KDCycleBannerView.h
+++ KDCycleBannerView/KDCycleBannerView.h
@@ -40,6 +40,7 @@ typedef void(^CompleteBlock)(void);
 
 @property (assign, nonatomic, getter = isContinuous) BOOL continuous;   // if YES, then bannerview will show like a carousel, default is NO
 @property (assign, nonatomic) NSUInteger autoPlayTimeInterval;  // if autoPlayTimeInterval more than 0, the bannerView will autoplay with autoPlayTimeInterval value space, default is 0
+@property (strong, readonly) UIPageControl *pageControl;
 
 - (void)reloadDataWithCompleteBlock:(CompleteBlock)competeBlock;
 - (void)setCurrentPage:(NSInteger)currentPage animated:(BOOL)animated;
